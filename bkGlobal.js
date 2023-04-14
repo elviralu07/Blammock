4b2ead44c27190343b574ca9066a7ea295644ca0
roryvisuals <anrory21@gmail.com>
2023-04-14 16:33:32 -0700

    Basic global layout

---
+++ bkGlobal.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#top").on("click", function () {
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth",
+        })
+    })
+})
