ff7cfc879b70ae17a5d98bada9773a744b5897f5
roryvisuals <anrory21@gmail.com>
2023-04-26 14:33:14 -0700

    Barebones shopping cart

--- bkGlobal.js
+++ bkGlobal.js
@@ -1,6 +1,17 @@
+var cartItems = new Map();
+localStorage.setItem("cartItems", JSON.stringify(cartItems));
+var storedItems = JSON.parse(localStorage.getItem("cartItems"));
+
+const addItem = function() {
+    cartItems.set("item1", {
+        color: "red",
+        quantity: "3"
+    })
+}
+
 $(document).ready(function () {
     console.log("hello");
-    
+
     $("#top").on("click", function() {
         window.scroll({
             top: 0,
@@ -11,6 +22,16 @@ $(document).ready(function () {
 
     $("#cart-toggle").on("click", function() {
         console.log("hello");
-        $(".sidebar").css("style", "0px");
+        $("#sidebar").css("right", "0px");
+    })
+
+    $("#cart-close").on("click", function() {
+        console.log("hello");
+        $("#sidebar").css("right", "-500px");
+    })
+
+    $("#add-to-cart-btn").on("click", function() {
+        addItem();
+        console.log(cartItems);
     })
 })
