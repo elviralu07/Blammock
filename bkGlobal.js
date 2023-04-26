f31096c4caeeb5a9e8058799cfb7da2a89b52830
roryvisuals <anrory21@gmail.com>
2023-04-26 13:17:12 -0700

    Fix JS

--- bkGlobal.js
+++ bkGlobal.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
-    $("#top").on("click", function () {
+    console.log("hello");
+    
+    $("#top").on("click", function() {
         window.scroll({
             top: 0,
             left: 0,
             behavior: "smooth",
         })
+    })
+
+    $("#cart-toggle").on("click", function() {
+        console.log("hello");
+        $(".sidebar").css("style", "0px");
     })
 })
