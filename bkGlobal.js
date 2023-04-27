645d950b318b21c582b18be5e45ff514a1f6fe84
Khalil Mayden <mayden@usc.edu>
2023-04-27 11:03:53 -0700

    Revert "Merge branch 'main' of https://github.com/elviralu07/Blammock"
    This reverts commit 9412b184498772e83ade57be561ded162b14409f, reversing
    changes made to 2de8494aeadb18a79bebac11a4bfe25cd8157620.

--- bkGlobal.js
+++ bkGlobal.js
@@ -1,73 +1,50 @@
-import {
-    isValid,
-    isExpirationDateValid,
-    isSecurityCodeValid,
-    getCreditCardNameByNumber
-} from 'creditcard.js';
-
 const bmColors = {
     color1: ["Ivy Green", "darkolivegreen", "Green"],
     color2: ["Ocean Blue", "navy", "Blue"],
     color3: ["Earth Brown", "sienna", "Red"]
 }
 
-let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
-let counter = cartItems.length;
-console.log(cartItems);
+console.log(JSON.parse(localStorage.getItem("cartItems")));
+var cartItems = new Map(JSON.parse(localStorage.getItem("cartItems"))) || new Map();
+localStorage.setItem("cartItems", JSON.stringify([...cartItems]));
+var counter = cartItems.size;
+console.log(counter)
 
-let qNum = 1;
-let color = "";
-let cartCode = "";
-let subtotal = "";
+var qNum = 1;
+var color = "";
+var cartCode = "";
 
 const addItem = function(userColor, userQuantity) {
     return new Promise(function(resolve, reject) {
         counter++;
-        cartItems.push({
+        cartItems.set(counter, {
             color: userColor,
             quantity: userQuantity
-        });
+        })
         qNum = 1;
         $("#quantity").html(qNum);
 
-        localStorage.setItem("cartItems", JSON.stringify(cartItems));
-        calcTotal();
-        resolve();
-    })
-}
+        localStorage.setItem("cartItems", JSON.stringify([...cartItems]));
 
-const removeItem = function(index) {
-    return new Promise(function(resolve, reject) {
-        counter--;
-        cartItems.splice(index, 1);
-
-        localStorage.setItem("cartItems", JSON.stringify(cartItems));
-        calcTotal();
-        
         resolve();
     })
 }
 
 const updateCart = function() {
+    console.log(cartItems);
     cartCode = "";
-    for(let i = 0; i < counter; i++) {
-        cartCode += "<div class='in-cart-item'>";
-        cartCode += "<span><strong>Blammock</strong></span> <br />";
-        cartCode += "<span>Color: " + cartItems[i].color[0] + "</span> <br />";
-        cartCode += "<span>Quantity: " + cartItems[i].quantity + "</span> <br />";
-        cartCode += "<span class='remove-item-bttn' data-index='" + i + "'>X</span>";
+    for(let i=1; i<=counter; i++) {
+        cartCode += "<div class='inCartItem'>"
+        cartCode += "<span>Blammock</span> <br />";
+        cartCode += "<span>Color: " + cartItems.get(i).color[0] + "</span> <br />";
+        cartCode += "<span>Quantity: " + cartItems.get(i).quantity + "</span> <br />";
         cartCode += "</div>";
-    }
-    if(cartCode == "") {
-        cartCode = "Your cart is empty";
-    } else {
-        cartCode += "<br /><hr> <a href='bkCheckout.html' id='checkout-bttn'>CHECKOUT</a> <br /><br /><br />"
     }
     $("#cart").html(cartCode);
 }
 
-const calcTotal = function() {
-    subtotal = counter * 60;
+const emptyCart = function() {
+    $("#cart").html("You have no items");
 }
 
 $(document).ready(function() {
@@ -86,39 +63,33 @@ $(document).ready(function() {
     })
 
     $("#cart-close").on("click", function() {
-        $("#sidebar").css("right", "-400px");
+        $("#sidebar").css("right", "-500px");
     })
 
     $("#add-to-cart-btn").on("click", function() {
-        if (color == "") {
+        if(color == "") {
             color = bmColors["color1"];
         }
         addItem(color, qNum).then(function() {
             updateCart();
         })
+        console.log(cartItems);
     })
 
     $(".option-c").on("click", function() {
-        const x = this.id;
+        let x = this.id;
         color = bmColors[x];
-        $("#main-display").css("background-image", "url('media/bk" + color[2] + ".jpeg')");
+        console.log(color[0]);
     })
 
     $(".quantity-counter").on("click", function() {
         if(this.value == "-" && qNum > 1) {
             qNum--;
-        } else if(this.value == "+") {
+        } else if (this.value == "+") {
             qNum++;
         } else {
             console.log("error")
         }
-        $("#quantity").html(qNum)
-    })
-
-    $(document).on("click", ".remove-item-bttn", function() {
-        const index = $(this).data("index");
-        removeItem(index).then(function() {
-            updateCart();
-        })
+        $("#quantity").html(qNum);
     })
 })
